d6652256a1f246c2dbe518a6b56e47fe016b633a
Van Kichline <vkichline@hotmail.com>
2017-11-28 20:09:36 -0800

    up and running on the Mac

---
+++ src/components/App/App.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import './App.css';
+import { setInterval } from 'timers';
+
+const logo = require('./logo.svg');
+
+interface ClockState {
+  time: Date;
+}
+
+class App extends React.Component<{}, ClockState> {
+  secondsTimer: NodeJS.Timer;
+
+  constructor(props: object) {
+    super(props);
+    this.state = { time: new Date() };
+  }
+
+  refreshTime() {
+    this.setState({ time: new Date() });
+  }
+
+componentDidMount() {
+    this.secondsTimer = setInterval(() => this.refreshTime(), 1000);
+  }
+
+  componentWillUnmount() {
+     clearInterval(this.secondsTimer);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          The time is {this.state.time.toTimeString()}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
