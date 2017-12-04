810722a2f0005ae7af6da4082e4ec8fdd64f29f1
Van Kichline <vkichline@hotmail.com>
2017-12-03 16:06:19 -0800

    Improved clock face

--- src/components/ClockFace/ClockFace.tsx
+++ src/components/ClockFace/ClockFace.tsx
@@ -10,7 +10,13 @@ class ClockFace extends React.Component<ClockProps, {}> {
     super(props);
   }
 
-  formatTime(time: Date): string {
+  // Return an object with these parts:
+  // {
+  //   time:    string; // HH:MM
+  //   seconds: string; // SS
+  //   suffix:  string; //AM|PM
+  // }
+  getTimeParts(time: Date): { time: string, seconds: string, suffix: string} {
       function part2string(t: number): string {
         if (t < 10) {
             return '0' + t.toString();
@@ -27,13 +33,22 @@ class ClockFace extends React.Component<ClockProps, {}> {
       if (0 === hour) {
           hour = 12;
       }
-      return hour.toString() + ':' + part2string(minute) + ':' + part2string(second);
+      return { 
+        time: (hour.toString() + ':' + part2string(minute)),
+        seconds: part2string(second),
+        suffix: ispm ? 'PM' : 'AM'
+      };
   }
 
   render() {
+    let timeParts = this.getTimeParts(this.props.time);
     return (
       <div className="clock-face">
-        {this.formatTime(this.props.time)}
+        <div className="time">{timeParts.time}</div>
+        <div className="right-box">
+          <div className="seconds">{':' + timeParts.seconds}</div>
+          <div className="suffix">{timeParts.suffix}</div>
+        </div>
       </div>
     );
   }
